d5eaea340994a64bcfb429d6186ffcc0f63191c1
pinksideofthemoon <pinksideofthemoon@github.com>
2017-10-18 12:49:09 +0200

    Creación de la barra

--- logic.js
+++ logic.js
@@ -3,18 +3,24 @@ var bloques;    //Grupo de bloques para dibujar
 var bolas;      //Grupo para las bolas
 var bola_1;     //Bola del Jugador 1
 var bola_2;     //Bola del Jugador 2
-var barra_2;      //Barra del Jugador 1
+var barra_2;    //Barra del Jugador 1
 var barra_2;    //Barra del Jugador 2
 
 //Funcion de precarga de los sprites y objetos
 function preload(){
     game.load.image('Bola', 'assets/Bola.png');
+    game.load.spritesheet('Bloques', 'assets/Bloques.png', 32, 8);
 }
 
 //Funcion de creacion de las variables a usar, grupos etc
 function create(){
     game.physics.startSystem(Phaser.Physics.ARCADE);
     
+    //bloques
+    bloques = game.add.sprite(32, game.world.height - 150, 'Bloques');
+    bloques.scale.setTo(2, 2);
+    bloques.frame= 0;
+
     //Creamos el grupo "balls" y activamos su física
     bolas = game.add.group();
     bolas.enableBody = true;
