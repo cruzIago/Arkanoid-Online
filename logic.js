7173b585a45b0c0a9419433d902c3b4218773f0c
Alexander Matos Teodoro <a.matost@alumnos.urjc.es>
2017-10-18 12:20:34 +0200

    test bloque

--- logic.js
+++ logic.js
@@ -3,14 +3,15 @@ var bloques; //Grupo de bloques para dibujar
 
 //Funcion de precarga de los sprites y objetos
 function preload(){
-    game.load.image('Bloques', 'assets/Bloques.png');
+    
     game.load.image('Ball', 'assets/Ball.png');
+    game.load.spritesheet('bloques', 'assets/Bloques.png', 32, 8);
 }
 
 //Funcion de creacion de las variables a usar, grupos etc
 function create(){
 
-
+exBlock = game.add.sprite(32, game.world.height - 150, 'dude');
 }
 
 //Función de actualización de los sistemas de juego (movimientos, fisicas, etc)
