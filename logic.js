29da67270d173ee576a77fee968442cf1580a99c
Iago <iagocruzgarcia@gmail.com>
2017-10-18 12:06:27 +0200

    Sigue formando el esqueleto

--- logic.js
+++ logic.js
@@ -1,1 +1,18 @@
 var game= new Phaser.Game(800,600,Phaser.AUTO,'',{preload:preload,create:create,update:update});
+var bloques; //Grupo de bloques para dibujar
+
+//Funcion de precarga de los sprites y objetos
+function preload(){
+}
+
+//Funcion de creacion de las variables a usar, grupos etc
+function create(){
+
+
+}
+
+//Función de actualización de los sistemas de juego (movimientos, fisicas, etc)
+function update(){
+
+
+}
